4776d0fa405294c6e709245bc3b10795f9dc55e8
himanoa <matsunoappy@gmail.com>
2021-01-20 16:23:16 +0900

    WIP

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,8 @@
-console.log("hello")
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const elm = document.getElementById("root");
+
+if(elm){
+  ReactDOM.render(<p>Hello world</p>, elm)
+}
