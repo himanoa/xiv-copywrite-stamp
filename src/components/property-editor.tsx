c65dec1d2e6ce8aaa1046b715eb6df69f3d5b63e
himanoa <matsunoappy@gmail.com>
2021-01-20 21:50:29 +0900

    WIP

--- src/components/property-editor.tsx
+++ src/components/property-editor.tsx
@@ -5,18 +5,28 @@ interface Props {
 
 }
 
+interface CopyrightPositionRadioGroupProps {
+  onCopyrightPositionChange: (e: React.FormEvent<HTMLInputElement>) => void
+}
+
+export const CopyrightPositionRadioGroup = (props: CopyrightPositionRadioGroupProps) => {
+  return (
+    <RadioGroup label={"コピーライトの位置"} onChange={props.onCopyrightPositionChange}>
+      <Radio label="左上" value="upper-left"/>
+      <Radio label="左下" value="lower-left"/>
+      <Radio label="右上" value="upper-right"/>
+      <Radio label="右下" value="lower-right"/>
+    </RadioGroup>
+  )
+}
+
 export const PropertyEditor = (props: Props) => {
-  const onCopyRightPositionChange = useCallback(() => {
-    // TODO: implement
+  const onCopyrightPositionChange = useCallback((v) => {
+    console.dir(v.target.value)
   }, [])
   return (
     <form>
-      <RadioGroup label={"コピーライトの位置"} onChange={onCopyRightPositionChange}>
-        <Radio label="左上" value="upper-left"/>
-        <Radio label="左下" value="lower-left"/>
-        <Radio label="右上" value="upper-right"/>
-        <Radio label="右下" value="lower-right"/>
-      </RadioGroup>
+      <CopyrightPositionRadioGroup onCopyrightPositionChange={onCopyrightPositionChange}/>
     </form>
   )
 }
