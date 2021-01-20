43889b33d37222a6b9f5eac174e978c0b1ef2d03
himanoa <matsunoappy@gmail.com>
2021-01-20 17:06:37 +0900

    WIP

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Classes, Intent, Spinner } from "@blueprintjs/core";
 import { Content } from './components/content'
 
 const elm = document.getElementById("root");
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,9 @@ module.exports = ({
       global: {}
     })
   ],
+  resolve: {
+    extensions: [".wasm", ".mjs", ".js", ".jsx", ".ts", ".tsx", ".json"],
+  },
   devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -27,7 +30,7 @@ module.exports = ({
   },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 8080
+    port: 8080,
+    hot: true,
   }
 });
