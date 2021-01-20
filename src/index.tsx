e4dd48a1349c38e2cedd0274b3034ccdbb0ec7f9
himanoa <matsunoappy@gmail.com>
2021-01-20 16:36:05 +0900

    WIP

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Classes, Intent, Spinner } from "@blueprintjs/core";
 
 const elm = document.getElementById("root");
 
 if(elm){
-  ReactDOM.render(<p>Hello world</p>, elm)
+  ReactDOM.render(<Spinner className={Classes.SMALL} intent={Intent.PRIMARY}/>, elm)
 }
