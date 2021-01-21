6cd2cb358d3ad26750929ca9ca223781a322724f
himanoa <matsunoappy@gmail.com>
2021-01-22 00:32:22 +0900

    Use canvas textBaseline option

--- src/draw-text.ts
+++ src/draw-text.ts
@@ -2,8 +2,9 @@ import { Position } from "./text-property"
 
 export type DrawTextParameter = {
   text: string;
-  x: number
-  y: number
+  x: number;
+  y: number;
+  baseLine: CanvasTextBaseline
 }
 
 export type Size = {
@@ -15,16 +16,16 @@ export const getDrawTextParameter = (text: string, position: Position, textSize:
   const padding = 8;
   switch(position) {
     case "upper-left": {
-      return { x: 0 + padding, y: 0 + textSize.height + padding, text }
+      return { x: 0 + padding, y: 0 + textSize.height / 2 + padding, baseLine: 'bottom',text  }
     };
     case "upper-right": {
-      return { x: imageSize.width - textSize.width - padding, y: 0 + textSize.height + padding, text }
+      return { x: imageSize.width - textSize.width - padding, y: 0 + textSize.height / 2 + padding, baseLine: 'bottom', text }
     };
     case "lower-left": {
-      return { x: 0 + padding, y: imageSize.height - textSize.height - padding, text }
+      return { x: 0 + padding, y: imageSize.height - textSize.height / 2, baseLine: 'top', text }
     };
     case "lower-right": {
-      return { x: imageSize.width - textSize.width - padding, y: imageSize.height - textSize.height - padding, text }
+      return { x: imageSize.width - textSize.width - padding, y: imageSize.height - textSize.height / 2, baseLine: 'top', text }
     };
   }
 }
