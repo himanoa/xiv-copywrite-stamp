035482b9180aa1853eb736ff1efc8aefa68bcdec
himanoa <matsunoappy@gmail.com>
2021-01-21 17:32:12 +0900

    WIP

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Navbar, NavbarGroup, NavbarHeading, NavbarDivider, Alignment, Colors } from '@blueprintjs/core'
+import { Navbar, NavbarGroup } from '@blueprintjs/core'
+import styled from 'styled-components'
 
 interface Props {
 
@@ -8,8 +9,9 @@ interface Props {
 export const Footer = (props: Props) => {
   return (
     <Navbar>
-      <NavbarGroup>
-        <p>記載されている会社名・製品名・システム名などは、各社の商標、または登録商標です。 Copyright (C) 2010 SQUARE ENIX CO., LTD. All Rights Reserve  © 2021 HIMANOA </p>
+      <NavbarGroup align="center">
+        <p>記載されている会社名・製品名・システム名などは、各社の商標、または登録商標です。</p>
+        <p>Copyright (C) 2010 SQUARE ENIX CO., LTD. All Rights Reserve  © 2021 HIMANOA </p>
       </NavbarGroup>
     </Navbar>
   )
